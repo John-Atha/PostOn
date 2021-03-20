68692f8902d898d9a07554206ed30acd9ea3c325
john_atha <giannisj3@gmail.com>
2021-03-20 15:51:26 +0200

    Add specific-user posts statistics endpoints

--- network/views.py
+++ network/views.py
@@ -1313,7 +1313,6 @@ def UsersDailyCommentsStats(request, id):
         except User.DoesNotExist:
             return JsonResponse({"error": "Invalid user id"}, status=400)
         
-
 def UsersMonthlyCommentsStats(request, id):
     if request.method!="GET":
         return JsonResponse({"error": "Only GET method is allowed"}, status=400)
@@ -1352,6 +1351,72 @@ def UsersMonthlyCommentsStats(request, id):
             )
             if len(stats)==0:
                 return JsonResponse({"error": "No comments found"}, status=402)
+            return JsonResponse(stats, safe=False, status=200)
+        except User.DoesNotExist:
+            return JsonResponse({"error": "Invalid user id"}, status=400)
+
+def UsersDailyPostsStats(request, id):
+    if request.method!="GET":
+        return JsonResponse({"error": "Only GET method is allowed"}, status=400)
+    else:
+        try:
+            user = User.objects.get(id=id)
+            posts = Post.objects.filter(owner=user)
+            postsCount = {
+                "Monday": 0,
+                "Tuesday": 0, 
+                "Wednesday": 0, 
+                "Thursday": 0, 
+                "Friday": 0, 
+                "Saturday": 0, 
+                "Sunday": 0
+            }
+            for post in posts:
+                s = str(post.date).split(' ')[0].split('-')
+                day = datetime(int(s[0]), int(s[1]), int(s[2])).strftime("%A")
+                postsCount[day] = postsCount[day]+1
+            return JsonResponse(postsCount, safe=False, status=200)
+        except User.DoesNotExist:
+            return JsonResponse({"error": "Invalid user id"}, status=400)
+
+def UsersMonthlyPostsStats(request, id):
+    if request.method!="GET":
+        return JsonResponse({"error": "Only GET method is allowed"}, status=400)
+    else:
+        try:
+            user = User.objects.get(id=id)
+            posts = Post.objects.filter(owner=user).order_by('date')
+            stats = []
+            prevYearMonth = str(posts[0].date).split('-')[0]+'-'+str(posts[0].date).split('-')[1]
+            counter = -1
+            for post in posts:
+                try:
+                    date = str(post.date).split('-')
+                    yearMonth = date[0]+'-'+date[1]
+                    if yearMonth!=prevYearMonth and prevYearMonth!='':
+                        stats.append(
+                            {
+                                "year-month": prevYearMonth,
+                                "posts": counter
+                            }
+                        )
+                        counter=0
+                        prevYearMonth = yearMonth
+                    elif prevYearMonth=="":
+                        counter=0
+                        prevYearMonth = yearMonth
+                    else:
+                        counter = counter+1
+                except Exception:
+                    pass
+            stats.append(
+                {
+                    "year-month": prevYearMonth,
+                    "posts": counter
+                }
+            )
+            if len(stats)==0:
+                return JsonResponse({"error": "No posts found"}, status=402)
             return JsonResponse(stats, safe=False, status=200)
         except User.DoesNotExist:
             return JsonResponse({"error": "Invalid user id"}, status=400)
