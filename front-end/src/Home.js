fd6e366268f318f3cd4017e9f8814b8edaca906d
john_atha <giannisj3@gmail.com>
2021-03-22 21:16:14 +0200

    Add home css and basic auth check

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import './Home.css';
-
 import MyNavbar from './MyNavbar'
 
+import {isLogged} from './api';
+
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userId: null,
+            logged: null,
+            error: null,
+        }
+    }
+
+    componentDidMount() {
+        isLogged()
+        .then(response => {
+            console.log(response)
+            this.setState({
+                userId: response.data.id,
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            this.setState({
+                error: "Not logged in"
+            })
+        })
+    }
 
     render() {
         return (
             <div className="all-page">
                 <MyNavbar />
+                <div className="error-message">{this.state.error}</div>
+                <div className="success-message">{this.state.userId}</div>
             </div>
         )
     }
