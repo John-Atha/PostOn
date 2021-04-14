92bebbef5c11e7f20edd068a1a72446669139c7b
John-Atha <giannisj3@gmail.com>
2021-04-14 20:37:18 +0300

    Add mentions post on new comment

--- front-end/src/Comments.js
+++ front-end/src/Comments.js
@@ -4,7 +4,7 @@ import like_icon from './images/like.png';
 import liked_icon from './images/liked.png';
 import delete_icon from './images/delete-icon.png';
 import Likes from './Likes';
-import {getUsers, getPostsComments, getLikesSample, getAllLikes, LikeComment, UnLikeComment, DeleteComment, AddComment, getUser, UserLikesComment} from './api';
+import {getUsers, getPostsComments, getLikesSample, getAllLikes, LikeComment, UnLikeComment, DeleteComment, AddComment, getUser, UserLikesComment, PostCommentTag} from './api';
 import ProfileCard from  './ProfileCard';
 import 'react-notifications-component/dist/theme.css'
 import { store } from 'react-notifications-component';
@@ -25,11 +25,15 @@ class NewComment extends React.Component {
             postId: this.props.postId,
             owner: this.props.owner,
             error: null,
+            tagsToPost: [],
+            newId: null,
         }
         this.handleInput = this.handleInput.bind(this);
         this.submit = this.submit.bind(this);
         this.getUserInfo = this.getUserInfo.bind(this);
         this.askTags = this.askTags.bind(this);
+        this.filterComment = this.filterComment.bind(this);
+        this.addTags = this.addTags.bind(this);
     }
     createNotification = (type, title="aaa", message="aaa") => {
         console.log("creating notification");
@@ -72,6 +76,109 @@ class NewComment extends React.Component {
             })
         }
     }
+    filterComment = (text) => {
+        console.log("users i see")
+        console.log(this.state.usersList)
+        console.log("i am filter comment");
+        let post_text = text;
+        console.log("initial text")
+        let final_post_object = [];
+        let s3 = [];
+        post_text = post_text.replaceAll(")@", ") @");
+        console.log(post_text);
+        //let s2 = post_text.trim().split(/\s+/);
+        let s2 = post_text.split(' ');
+        for (let i=0; i<s2.length; i++) {
+            s2[i]+=' ';
+        }
+        console.log("after fixing spaces")
+        console.log(s2)
+        for (let i=0; i<s2.length; i++) {
+            if (s2[i]!==[' ']) {
+                console.log("sublist")
+                console.log(s2[i])
+                let subList = s2[i].split('\n');
+                console.log(subList)
+                if (subList.length>1) {
+                    for (let j=0; j<subList.length-1; j++) {
+                        if (!subList[j].endsWith('\n')) {
+                            subList[j]+='\n';
+                        }    
+                        s3.push(subList[j]);
+                    }
+                    s3.push(subList[subList.length-1])
+                }
+                else {
+                    s3.push(subList);
+                }
+            }
+        }
+        console.log("BROKEN LIST")
+        //console.log(s2);
+        s3 = s3.flat();
+        console.log(s3);
+        let index=0;
+        s3.forEach(el => {
+            console.log(el)
+            if (el.startsWith('@')) {    
+                let matched = false;
+                this.state.usersList.forEach(suggest => {
+                    let sugg=suggest.display;
+                    if (el.startsWith(`@[${sugg}]`)) {
+                        matched = true;
+                        console.log(`el: ${el}`)
+                        let el2 = el.split(')')
+                        //console.log(`el parts: ${el2}`)
+                        let first = el2[0]
+                        let dump = el2[1]
+                        //console.log(`first: ${first}`)
+                        //let username = first.split(']')[0].slice(2)
+                        //let id = first.split(']')[1].slice(1)
+                        console.log(`username: ${suggest.display}`)
+                        console.log(`id: ${suggest.id}`)
+                        console.log(`dump: ${dump}`)
+                        final_post_object.push({
+                            "tag": {
+                                "username": suggest.display,
+                                "id": suggest.id,
+                            },
+                            "dump": dump,
+                        })
+                        index++;
+                    }
+                })
+            }
+        })
+        console.log("Tags to post:")
+        console.log(final_post_object)
+        this.setState({
+            tagsToPost: final_post_object,
+        })
+    }
+
+    addTags = (text) => {
+        // gets post id from: this.state.newId
+        // gets tagsList from: this.state.tagsToPost
+        this.filterComment(text);
+        setTimeout(()=> {
+            this.state.tagsToPost.forEach(obj => {
+                let id = obj.tag.id;
+                let object = {
+                    "mentioned": {
+                        "id": id,
+                    }
+                }
+                PostCommentTag(this.state.newId, object)
+                .then(response => {
+                    console.log(response);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+            })
+        }, 200)
+    }
+
     handleInput = (event) => {
         const value = event.target.value;
         this.setState({
@@ -86,9 +193,12 @@ class NewComment extends React.Component {
             .then(response => {
                 console.log(response);
                 this.props.updateComments();
+                let prevText = this.state.text;
                 this.setState({
                     text: "",
+                    newId: response.data.id,
                 })
+                setTimeout(()=>{this.addTags(prevText);}, 1000);
                 this.createNotification('success', 'Hello,', 'Comment posted succesffully');
             })
             .catch(err => {
